551b1268f99639f3b3ff7014f6d74dc9a494e82a
Benoit Borrel <benoit@borrel.com>
2025-11-16 14:10:25 -0500

    Fixtures: add demo data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,8 +2,10 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Pet;
 use App\Entity\PetBreed;
 use App\Entity\PetType;
+use App\Entity\Sex;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -20,15 +22,17 @@ class AppFixtures extends Fixture
         }
 
         // Cat's breeds
-        foreach (['Persian', 'Siamese'] as $breedName) {
+        foreach (['Unknown', 'Siamese', 'Tabby'] as $breedName) {
             $petBreed = new PetBreed();
             $petBreed->setName($breedName);
             $petBreed->setType($this->getReference('Cat', PetType::class));
             $petBreed->setIsDangerous(false);
             $manager->persist($petBreed);
+            $this->addReference($breedName, $petBreed);
         }
+
         // Dog's breeds
-        foreach (['Beagle', 'Labrador', 'Mastiff', 'Pitbull'] as $breedName) {
+        foreach (['Unknown', 'Beagle', 'Labrador', 'Mastiff', 'Pitbull'] as $breedName) {
             $petBreed = new PetBreed();
             $petBreed->setName($breedName);
             $petBreed->setType($this->getReference('Dog', PetType::class));
@@ -37,6 +41,47 @@ class AppFixtures extends Fixture
                 $petBreed->setIsDangerous(true);
             }
             $manager->persist($petBreed);
+            $this->setReference($breedName, $petBreed);
+        }
+
+        // Demo data
+        $pets = [
+            'Minka' => [
+                'setType' => $this->getReference('Cat', PetType::class),
+                'addBreed' => $this->getReference('Tabby', PetBreed::class),
+                'setName' => 'Minka',
+                'setApproximateAge' => 3.5,
+                'setSex' => Sex::Female,
+            ],
+            'Taupi' => [
+                'setType' => $this->getReference('Cat', PetType::class),
+                'addBreed' => $this->getReference('Unknown', PetBreed::class),
+                'setName' => 'Taupi',
+                'setDateOfBirth' => new \DateTimeImmutable('2024-04-02'),
+                'setSex' => Sex::Female,
+            ],
+            'Doggy' => [
+                'setType' => $this->getReference('Dog', PetType::class),
+                'addBreed' => [
+                    $this->getReference('Unknown', PetBreed::class),
+                    $this->getReference('Pitbull', PetBreed::class),
+                ],
+                'setName' => 'Doggy',
+                'setSex' => Sex::Male,
+            ],
+        ];
+        foreach ($pets as $petData) {
+            $pet = new Pet();
+            foreach ($petData as $method => $value) {
+                if (is_array($value)) {
+                    foreach ($value as $v) {
+                        $pet->{$method}($v);
+                    }
+                } else {
+                    $pet->{$method}($value);
+                }
+            }
+            $manager->persist($pet);
         }
 
         $manager->flush();
