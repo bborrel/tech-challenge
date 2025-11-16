2e83d88f7e4baead5170abad2be9fa61633cf4b9
Benoit Borrel <benoit@borrel.com>
2025-11-15 20:08:41 -0500

    Fixtures: add data for constant tables

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,8 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\PetBreed;
+use App\Entity\PetType;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +11,33 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Pet types
+        foreach (['Cat', 'Dog'] as $typeName) {
+            $petType = new PetType();
+            $petType->setName($typeName);
+            $manager->persist($petType);
+            $this->addReference($typeName, $petType);
+        }
+
+        // Cat's breeds
+        foreach (['Persian', 'Siamese'] as $breedName) {
+            $petBreed = new PetBreed();
+            $petBreed->setName($breedName);
+            $petBreed->setType($this->getReference('Cat', PetType::class));
+            $petBreed->setIsDangerous(false);
+            $manager->persist($petBreed);
+        }
+        // Dog's breeds
+        foreach (['Beagle', 'Labrador', 'Mastiff', 'Pitbull'] as $breedName) {
+            $petBreed = new PetBreed();
+            $petBreed->setName($breedName);
+            $petBreed->setType($this->getReference('Dog', PetType::class));
+            $petBreed->setIsDangerous(false);
+            if ('Pitbull' === $breedName) {
+                $petBreed->setIsDangerous(true);
+            }
+            $manager->persist($petBreed);
+        }
 
         $manager->flush();
     }
